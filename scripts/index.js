253ff17202b0c01305df6d15c44e6d2a17cb00fe
Albina Makarova <albinamakarova@rambler.ru>
2023-02-25 05:07:33 +0300

    1st review edits

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,20 @@
 const editButton = document.querySelector('.profile__edit-button');
-console.log(editButton)
 
 const addButton = document.querySelector('profile__add-button');
-console.log(addButton)
-
-let like = document.querySelector('.like');
-console.log(like)
-
-function onClick(like) {
-  like.className = "like_liked";
-};
 
 let userName = document.querySelector('.profile__name');
-console.log(userName)
 
 let userDescription = document.querySelector('.profile__description');
-console.log(userDescription)
 
 const popup = document.querySelector('.popup');
-console.log(popup)
 
 const popupContainer = document.querySelector('.popup__container')
-console.log(popupContainer)
 
 const closeButton = document.querySelector('.popup__close-button');
-console.log(closeButton)
 
-let addName = popupContainer.querySelector('.popup__name-field_input');
+let addName = popupContainer.querySelector('.popup__field_input_name');
 
-let addDescription = popupContainer.querySelector('.popup__description-field_input');
+let addDescription = popupContainer.querySelector('.popup__field_input_description');
 
 function openPopup() {
   popup.classList.add('popup_opened');
@@ -50,5 +36,4 @@ function handleFormSubmit(evt) {
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 popupContainer.addEventListener('submit', handleFormSubmit);
-like.addEventListener('click', onClick);
 
